133711c6f02d4ed3f550167740470d86c5520492
haohaodu <hdu2899@gmail.com>
2020-08-29 16:12:28 -0400

    sending data information back to server

--- index.js
+++ index.js
@@ -10,20 +10,30 @@ var serviceAccount = require("./service_account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://icontribute2-dbf5e.firebaseio.com",
+  databaseURL: "https://icontribute2-dbf5e.firebaseio.com"
 });
 
 const firestore = admin.firestore;
+
+app.get("/events", async (req, res) => {
+  console.log();
+  const snapshot = await firestore().collection("events").get();
+  let newList = [];
+  snapshot.forEach(doc => {
+    newList.push(doc.data());
+  });
+  console.log("new list: ", newList);
+  res.send(newList);
+});
 
 app.get("/user", async (req, res) => {
   await firestore()
     .collection("user")
     .get()
-    .then((elements) => {
-      console.log("firestore data call for user 1:");
-      console.log(elements.docs[0].data());
-      res.send(elements.docs[0].data())
-    }).catch(e => res.error('error retrieving information'));
+    .then(elements => {
+      res.send(elements.docs[0].data());
+    })
+    .catch(e => res.error("error retrieving information"));
 });
 
 app.listen(5000);
