c65ce57b2f3b0faaf9d548b908d29103eaac8781
medwardson <mnaidooe@uwaterloo.ca>
2020-08-29 13:58:56 -0400

    setup

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ const app = express();
 var admin = require("firebase-admin");
 app.use(bodyParser.json());
 
+var serviceAccount = require("./service_account.json");
 
-app.listen(3000)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://icontribute2-dbf5e.firebaseio.com",
+});
+
+const firestore = admin.firestore;
+
+app.get("/user", async (req, res) => {
+  await firestore()
+    .collection("user")
+    .get()
+    .then((elements) => {
+      console.log("firestore data call for user 1:");
+      console.log(elements.docs[0].data());
+      res.send(elements.docs[0].data())
+    }).catch(e => res.error('error retrieving information'));
+});
+
+app.listen(5000);
